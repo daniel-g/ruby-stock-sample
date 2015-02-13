9db06f552193e8c3b8c77fff641e6f6dcf459f6c
Daniel Gaytan <dgaytan6@gmail.com>
2015-02-13 13:14:35 -0600

    Fix: store best sell and buy stock instead of indexes

--- api.rb
+++ api.rb
@@ -1,31 +1,30 @@
 require 'sinatra'
 require 'logger'
 
-
 post '/stock' do
   logger = Logger.new(File.open('stock.log', 'a'))
-  best_revenue = 0
-  buy_index = 0
-  sell_index = 1
-  params[:data].each_with_index do |stock1, i|
-    stock1 = stock1.split(',')
-    stock1[1] = stock1[1].to_f
-    timestamp1, price1 = stock1[0], stock1[1]
+  best_revenue = ''
+  best_buy_stock, best_sell_stock = [], []
+  params[:data].each_with_index do |buy_stock, i|
+    buy_stock = buy_stock.split(',')
+    buy_stock[1] = buy_stock[1].to_f
+    timestamp1, price1 = buy_stock[0], buy_stock[1]
 
-    params[:data][(i + 1)..-1].each_with_index do |stock2, j|
-      stock2 = stock2.split(',')
-      stock2[1] = stock2[1].to_f
-      timestamp2, price2 = stock2[0], stock2[1]
+    params[:data][(i + 1)..-1].each_with_index do |sell_stock, j|
+      sell_stock = sell_stock.split(',')
+      sell_stock[1] = sell_stock[1].to_f
+      timestamp2, price2 = sell_stock[0], sell_stock[1]
 
-      best_revenue = price2 - price1 if best_revenue == 0
+      best_revenue = price2 - price1 if best_revenue == ''
 
       revenue = price2 - price1
       if revenue > best_revenue
-        buy_index, sell_index = i, j
+        best_buy_stock, best_sell_stock = buy_stock, sell_stock
+        best_revenue = price2 - price1
       end
     end
   end
 
-  logger.info "#{ params[:name] }, #{ params[:data][buy_index] }, #{ params[:data][sell_index] }"
+  logger.info "#{ params[:name] }, #{ best_buy_stock[0] }, #{ best_sell_stock[0] }"
   logger.close
 end
